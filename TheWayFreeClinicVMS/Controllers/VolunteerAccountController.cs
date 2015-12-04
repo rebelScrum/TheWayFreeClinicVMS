a3ba83142d601975196693a2901b81f358d5b29f
Nadiia Semenchuk <nadiiasem@gmail.com>
2015-12-04 02:28:06 -0500

    A little bit of changes to views

--- TheWayFreeClinicVMS/Controllers/VolunteerAccountController.cs
+++ TheWayFreeClinicVMS/Controllers/VolunteerAccountController.cs
@@ -1,89 +1,122 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using TheWayFreeClinicVMS.DataAccessLayer;
+using TheWayFreeClinicVMS.Models;
 
 namespace TheWayFreeClinicVMS.Controllers
 {
     public class VolunteerAccountController : Controller
     {
-        // GET: VolunteerAccount
-        public ActionResult Index()
+        private VMSContext db = new VMSContext();
+
+        // GET: VolunteerAccount/5
+        public ActionResult Index(int? id)
         {
-            return View();
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Volunteer volunteer = db.Volunteers.Find(id);
+            if (volunteer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(volunteer);
         }
 
         // GET: VolunteerAccount/Details/5
-        public ActionResult Details(int id)
+        public ActionResult Details(int? id)
         {
-            return View();
-        }
-
-        // GET: VolunteerAccount/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: VolunteerAccount/Create
-        [HttpPost]
-        public ActionResult Create(FormCollection collection)
-        {
-            try
+            if (id == null)
             {
-                // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            catch
+            Volunteer volunteer = db.Volunteers.Find(id);
+            if (volunteer == null)
             {
-                return View();
+                return HttpNotFound();
             }
+            return View(volunteer);
         }
+
+
 
         // GET: VolunteerAccount/Edit/5
-        public ActionResult Edit(int id)
+        public ActionResult Edit(int? id)
         {
-            return View();
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Volunteer volunteer = db.Volunteers.Find(id);
+            if (volunteer == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.volID = new SelectList(db.Econtacts, "volID", "ecFirstName", volunteer.volID);
+            ViewBag.volID = new SelectList(db.Licenses, "volID", "volID", volunteer.volID);
+            ViewBag.spcID = new SelectList(db.Specialties, "spcID", "spcName", volunteer.spcID);
+            return View(volunteer);
         }
 
         // POST: VolunteerAccount/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "volID,volFirstName,volLastName,volMiddleName,volDOB,volEmail,volPhone,volStreet1,volStreet2,volCity,volState,volZip,volStartDate,volActive,spcID")] Volunteer volunteer)
         {
-            try
+            if (ModelState.IsValid)
             {
-                // TODO: Add update logic here
-
+                db.Entry(volunteer).State = EntityState.Modified;
+                db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            catch
-            {
-                return View();
-            }
+            ViewBag.volID = new SelectList(db.Econtacts, "volID", "ecFirstName", volunteer.volID);
+            ViewBag.volID = new SelectList(db.Licenses, "volID", "volID", volunteer.volID);
+            ViewBag.spcID = new SelectList(db.Specialties, "spcID", "spcName", volunteer.spcID);
+            return View(volunteer);
         }
 
         // GET: VolunteerAccount/Delete/5
-        public ActionResult Delete(int id)
+        public ActionResult Delete(int? id)
         {
-            return View();
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Volunteer volunteer = db.Volunteers.Find(id);
+            if (volunteer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(volunteer);
         }
 
         // POST: VolunteerAccount/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
         {
-            try
-            {
-                // TODO: Add delete logic here
+            Volunteer volunteer = db.Volunteers.Find(id);
+            db.Volunteers.Remove(volunteer);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
 
-                return RedirectToAction("Index");
-            }
-            catch
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
             {
-                return View();
+                db.Dispose();
             }
+            base.Dispose(disposing);
         }
     }
 }

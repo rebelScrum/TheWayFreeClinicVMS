e8edfbd65654c75350e5482d2d98adb2fd013227
polelia <nadiiasem87@gmail.com>
2016-02-25 15:39:39 -0500

    Volunteer Updates

--- TheWayFreeClinicVMS/Models/Volunteer.cs
+++ TheWayFreeClinicVMS/Models/Volunteer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
